fd524c9d2cbb05a9304534a0cfe2438f14c68d4f
Mari <dennismaricarl@gmail.com>
2023-08-10 18:18:45 -0700

    wrote unit and functional tests

---
+++ block18.js
@@ -0,0 +1,26 @@
+//Unit Tests:
+
+// 1. 
+    //Expect multiplication(2, 3) to multiply 2 and 3.
+    //Expect multiplication(2, 3) to return a number as the output (output is 6). 
+    //Expect multiplication("two", 3) to return an error. 
+    //Expect multiplication ("two", "three") to return an error. 
+
+
+// 2.
+    //Expect that the function concatOdds will return only a single array. 
+    //Expect concatOdds[array1], [array2] will return an array of odd numbers in ascending order. 
+    //Expect both arrays to contain integers and not strings. 
+    //Expect concatOdds (["3", "2" etc.]) to result in an error.
+    //Expect that the output, a single array, will not contain duplicate integers. 
+
+
+// Functional Tests:
+
+//1. 
+    //When a user checks out as a guest, they should be prompted to create an account or log in. 
+    //When a user decides to log in with an incorrect email address and/or password, they should see an error and/or prompted to try again. 
+    //When a user creates an account, they should be given a list of requirements of what their password should contain. (ie. at least 1 uppercase letter, atleast 1 number, etc.)
+    //When a user checks out with an empty cart, they should see a message that says "your shopping cart is empty."
+    //When a user is done shopping and clicks their cart, they should first see a display/list of all their items to review. 
+    //When a user clicks "proceed to checkout", they should be prompted to choose a delivery method, input their shipping info and payment details. 
